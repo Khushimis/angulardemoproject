ceeaeb3868c83f3e51ede238a31af1afd2c78ee9
khushimis <khushimishra1104@gmail.com>
2020-09-09 18:16:41 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { AboutComponent } from './components/about/about.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { ReactiveformComponent } from './components/reactiveform/reactiveform.component';
+import { UserComponent } from './components/user/user.component';
+import { AddUserComponent} from './components/add-user/add-user.component';
+import { BindingDemoComponent } from './components/binding-demo/binding-demo.component';
+import { ClassdemoComponent } from './components/classdemo/classdemo.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path: 'contact',component:ContactsComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'emp', component: EmployeeComponent},
+  { path: 'list' , component: EmployeeListComponent},
+  { path: 'reactive-form', component:ReactiveformComponent},
+  { path: 'user', component:UserComponent},
+  { path: 'add-user',component:AddUserComponent},
+   { path: 'binding-Demo', component:BindingDemoComponent},
+   { path: 'classdemo', component:ClassdemoComponent},
+   { path: 'edit-user', component:EditUserComponent} 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { GenderpipePipe } from './pipes/genderpipe.pipe';
+import { AboutComponent } from './components/about/about.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { ReactiveformComponent } from './components/reactiveform/reactiveform.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { UserComponent } from './components/user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { BindingDemoComponent } from './components/binding-demo/binding-demo.component';
+
+import { ClassdemoComponent } from './components/classdemo/classdemo.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    EmployeeComponent,
+    GenderpipePipe,
+    AboutComponent,
+    EmployeeListComponent,
+    ReactiveformComponent,
+    UserComponent,
+    AddUserComponent,
+    BindingDemoComponent,
+    
+    ClassdemoComponent,
+    
+    EditUserComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
